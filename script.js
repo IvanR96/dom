526541f5a90dbaab23eb598942e8b48d29b3198a
IvanR96 <irios96@hotmail.com>
2024-06-05 23:22:51 -0700

    elements by tagname learned

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML = "I changed it!";
+//document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML = "I changed it!";
+
+
+document.getElementsByTagName('li')[2].innerHTML = "Changed a different way";
