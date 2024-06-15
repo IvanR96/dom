d0629ef8254b28d28b232a0ac1695481800378ab
IvanR96 <irios96@hotmail.com>
2024-06-14 23:21:47 -0700

    used innerhtml

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ document.querySelector('.list a').style.color = "red";
 
 document.querySelector('button').style.backgroundColor = "yellow";
 
-document.querySelector("h1").classList.add('huge');
+//document.querySelector("h1").classList.add('huge');
+
+document.querySelector('h1').innerHTML = "<em>Hello</em>";
